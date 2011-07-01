4cb7182125065c70210d44cc7d48b16dd773ac42
jagath janakiraman <jagath janakiraman@e5cf2fc6-503a-11dc-8e4b-3f561b9528bb>
2011-07-01 18:46:44 +0000

    CWS-4186: Added HTML and JSON support to search by inline collection definition
    git-svn-id: http://subversion.hpsvcs.com/services/IaaS/trunk/oocmdb@51845 e5cf2fc6-503a-11dc-8e4b-3f561b9528bb

--- src/main/java/com/palm/cloud/services/cmdb/resource/domain/SearchResource.java
+++ src/main/java/com/palm/cloud/services/cmdb/resource/domain/SearchResource.java
@@ -15,6 +15,8 @@ import javax.ws.rs.core.MediaType;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
+import com.palm.cloud.services.cmdb.collection.Node;
+import com.palm.cloud.services.cmdb.collection.xml.Vertex;
 import com.palm.cloud.services.cmdb.collection.xml.XMLParser;
 import com.palm.cloud.services.cmdb.condition.Condition;
 import com.palm.cloud.services.cmdb.domain.CIObject;
@@ -68,4 +70,27 @@ public class SearchResource extends AbstractBaseResource {
 		return new Viewable("/view/object/objects.jsp", objects);
 	}
 	
+	@Path("collection")
+	@POST
+	public List<Node> getCollectionByDefinition(Vertex vertex,
+			@QueryParam("offset") @DefaultValue("0") int offset,
+			@QueryParam("maxResults") @DefaultValue("100") int maxResults) {
+		
+		return collectionService.getCollectionByDefinition(vertex, 
+				offset, maxResults);
+	}
+	
+	@Path("collection")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public Viewable getCollectionByDefinitionAsHtml(
+			@FormParam("definition") String definition,
+			@FormParam("offset") @DefaultValue("0") int offset,
+			@FormParam("maxResults") @DefaultValue("100") int maxResults) {
+		
+		return new Viewable("/view/collection/nodes.jsp", collectionService
+				.getCollectionByXML(definition, offset, maxResults));
+	}
+
 }
