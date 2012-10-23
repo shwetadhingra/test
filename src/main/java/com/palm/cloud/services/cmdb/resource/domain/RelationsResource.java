a8defb69e6ab5a476090a4299aa81804adab6b77
hudson daemon <hudson daemon@e5cf2fc6-503a-11dc-8e4b-3f561b9528bb>
2012-10-23 23:20:50 +0000

    CWS-4186: Adding support to update resource
    git-svn-id: http://subversion.hpsvcs.com/services/IaaS/trunk/oocmdb@75629 e5cf2fc6-503a-11dc-8e4b-3f561b9528bb

--- src/main/java/com/palm/cloud/services/cmdb/resource/domain/RelationsResource.java
+++ src/main/java/com/palm/cloud/services/cmdb/resource/domain/RelationsResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -33,6 +34,12 @@ public class RelationsResource extends AbstractBaseResource {
 		cmdbDataService.addRelation(relation);
 	}
 
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateRelation(CIRelationship relation) {
+		cmdbDataService.updateRelation(relation);
+	}
+	
 	@Path("type/{type}")
 	@GET
 	public List<CIRelationship> getRelations(@PathParam("type") String type,
