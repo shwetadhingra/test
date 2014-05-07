761543500e0fa529d11d6b238e531812a2a05d62
jagath janakiraman <jagath janakiraman@e5cf2fc6-503a-11dc-8e4b-3f561b9528bb>
2014-05-07 01:56:28 +0000

    CWS-4186: Adding custom ServletContextListener to shutdown SharedThreadPool
    git-svn-id: http://subversion.hpsvcs.com/services/IaaS/trunk/oocmdb@163297 e5cf2fc6-503a-11dc-8e4b-3f561b9528bb

--- src/main/java/com/palm/cloud/services/cmdb/web/util/OOCMDBCleanupListener.java
+++ src/main/java/com/palm/cloud/services/cmdb/web/util/OOCMDBCleanupListener.java
@@ -1,20 +1,18 @@
-package com.palm.cloud.services.cmdb.request;
+package com.palm.cloud.services.cmdb.web.util;
 
 import javax.servlet.ServletContextEvent;
-
-import org.springframework.web.context.ContextLoaderListener;
+import javax.servlet.ServletContextListener;
 
 import com.palm.cloud.services.cmdb.collection.SharedThreadPool;
 
-public class OOCMDBContextLoaderListener extends ContextLoaderListener {
+public class OOCMDBCleanupListener implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent event) {
-		super.contextInitialized(event);
+		
 	}
 	
 	public void contextDestroyed(ServletContextEvent event) {
 		SharedThreadPool.getInstance().shutdown();
-		super.contextDestroyed(event);
 	}
 	
 }
